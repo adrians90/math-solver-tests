6a8d130abb859a2c13f471c73a9bf296f3f2c9a9
Adrian <adriansandu9090@gmail.com>
2024-03-07 11:54:07 +0000

    Testing Math-Solver with Jest
    In this exercise I practiced unit testing using Jest. I tested for all the use cases of the parse function in the Math Solver project.

---
+++ script.js
@@ -0,0 +1,21 @@
+import parse from "./parse.js"
+
+// function parse(equation) {
+//     const step = getNextStep()
+//     const result = solve(step)
+//     replaceNextStep(equation, step, result)
+
+// }
+
+const inputElement = document.getElementById("equation")
+const outputElement = document.getElementById("results")
+const form = document.getElementById("equation-form")
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const result = parse(inputElement.value)
+    outputElement.textContent = result
+})
+
